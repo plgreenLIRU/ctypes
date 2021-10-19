5706b77e402b8a6528fb3fe7ac0c8ca51b7d9a80
Pete Green <p.l.green@liverpool.ac.uk>
2021-10-19 13:56:14 +0100

    Python now uses arrays of lengh N

--- py_main.py
+++ py_main.py
@@ -8,11 +8,13 @@ c_lib = ctypes.CDLL(lib_name)
 # Define results type
 #c_lib.add.restype = ndpointer(dtype=ctypes.c_int, shape=(3,))
 
-# Add some arrays together
-x = (ctypes.c_int*3) (1, 2, 10)
-y = (ctypes.c_int*3) (0, 4, 1)
-z = (ctypes.c_int*3) (0, 0, 0)
-N = 3
+# Define arrays of size N
+N = 4
+x = (ctypes.c_int*N) (1, 2, 10, -1)
+y = (ctypes.c_int*N) (0, 4, 1, 1)
+z = (ctypes.c_int*N) (0, 0, 0, 0)
+
+# Add arrays together
 c_lib.add(x, y, z, (ctypes.c_int) (N))
 
 # Print result
