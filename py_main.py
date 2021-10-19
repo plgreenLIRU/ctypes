f19f5a175003d232ddd4c4cba643ae9f498fe2d3
Pete Green <p.l.green@liverpool.ac.uk>
2021-10-19 14:48:33 +0100

    Including N in inputs definition

--- py_main.py
+++ py_main.py
@@ -6,26 +6,21 @@ from numpy.ctypeslib import ndpointer
 lib_name = "C:\work\ctypes\peteLib.dll"
 c_lib = ctypes.CDLL(lib_name)
 
-# Define results type
-##c_lib.add.restype = ndpointer(dtype=ctypes.c_int, shape=(4,))
-c_lib.add.argtypes = [ndpointer(dtype=ctypes.c_int, shape=(4,)), 
-                      ndpointer(dtype=ctypes.c_int, shape=(4,)),
-                      ndpointer(dtype=ctypes.c_int, shape=(4,))]
 
 # Define arrays of size N
 N = 4
 x = np.array([1, 2, 10, -1])
 y = np.array([0, 4, 1, 1])
 z = np.array([0, 0, 0, 0])
-'''
-x = (ctypes.c_int*N) (1, 2, 10, -1)
-y = (ctypes.c_int*N) (0, 4, 1, 1)
-z = (ctypes.c_int*N) (0, 0, 0, 0)
-'''
+
+# Define results type
+c_lib.add.argtypes = [ndpointer(dtype=ctypes.c_int, shape=(N,)), 
+                      ndpointer(dtype=ctypes.c_int, shape=(N,)),
+                      ndpointer(dtype=ctypes.c_int, shape=(N,))]
+
 
 # Add arrays together
 c_lib.add(x, y, z, (ctypes.c_int) (N))
 
 # Print result
-for i in range(N):
-    print(z[i])
+print(z)
