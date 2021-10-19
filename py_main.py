748203f3a5f86bd695c1fd06284d090e77ffbbbb
Pete Green <p.l.green@liverpool.ac.uk>
2021-10-19 17:08:51 +0100

    Python code now works with float 32

--- py_main.py
+++ py_main.py
@@ -9,16 +9,16 @@ c_lib = ctypes.CDLL(lib_name)
 
 # Define arrays of, size N, that we want to add together
 N = 4
-x = np.array([1, 2, 10, -1])
-y = np.array([0, 4, 1, 1])
+x = np.array([0.1, 2, 10.1, -1], dtype=np.float32)
+y = np.array([0.6, 4.2, 1, 1], dtype=np.float32)
 
 # Initalise solution array, where we'll store x+y
-z = np.zeros([N], dtype=int)
+z = np.zeros(N, dtype=np.float32)
 
 # Define input types
-c_lib.add.argtypes = [ndpointer(dtype=ctypes.c_int, shape=(N,)),
-                      ndpointer(dtype=ctypes.c_int, shape=(N,)),
-                      ndpointer(dtype=ctypes.c_int, shape=(N,)),
+c_lib.add.argtypes = [ndpointer(dtype=ctypes.c_float, shape=(N,)),
+                      ndpointer(dtype=ctypes.c_float, shape=(N,)),
+                      ndpointer(dtype=ctypes.c_float, shape=(N,)),
                       ctypes.c_int]
 
 # Add arrays together
