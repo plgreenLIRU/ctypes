d6a72ba04e2e17ab411e41bcb8078a06cb4188ef
Pete Green <p.l.green@liverpool.ac.uk>
2021-10-19 13:54:39 +0100

    Python now prints results correctly

--- py_main.py
+++ py_main.py
@@ -6,12 +6,15 @@ lib_name = "C:\work\ctypes\peteLib.dll"
 c_lib = ctypes.CDLL(lib_name)
 
 # Define results type
-c_lib.add.restype = ndpointer(dtype=ctypes.c_int, shape=(3,))
+#c_lib.add.restype = ndpointer(dtype=ctypes.c_int, shape=(3,))
 
 # Add some arrays together
 x = (ctypes.c_int*3) (1, 2, 10)
 y = (ctypes.c_int*3) (0, 4, 1)
-res = c_lib.add(x, y)
+z = (ctypes.c_int*3) (0, 0, 0)
+N = 3
+c_lib.add(x, y, z, (ctypes.c_int) (N))
 
-# Print results
-print(res)
+# Print result
+for i in range(N):
+    print(z[i])
